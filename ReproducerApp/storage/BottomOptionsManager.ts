d4afc6fc7f12a3541ad4727112c2df500861e5ad
pigeonmal <40658543+pigeonmal@users.noreply.github.com>
2024-04-24 21:49:17 +0200

    Update BottomOptionsManager.ts

--- ReproducerApp/storage/BottomOptionsManager.ts
+++ ReproducerApp/storage/BottomOptionsManager.ts
@@ -1,7 +1,7 @@
 interface INativeSearch {
   hi: string;
 }
-import DefaultVariableStorage from "../storage/DefaultVariableStorage";
+import DefaultVariableStorage from "./DefaultVariableStorage";
 
 class BottomOptManger extends DefaultVariableStorage<INativeSearch | undefined> {
 	setStorage(value: INativeSearch | undefined, refresh: boolean = true) {
