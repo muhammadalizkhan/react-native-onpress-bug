3a92d0768fbfb1bc35c51578ce18e806209e7f8b
pigeonmal <40658543+pigeonmal@users.noreply.github.com>
2024-04-24 21:49:04 +0200

    Create BottomOptionsManager.ts

---
+++ ReproducerApp/storage/BottomOptionsManager.ts
@@ -0,0 +1,15 @@
+interface INativeSearch {
+  hi: string;
+}
+import DefaultVariableStorage from "../storage/DefaultVariableStorage";
+
+class BottomOptManger extends DefaultVariableStorage<INativeSearch | undefined> {
+	setStorage(value: INativeSearch | undefined, refresh: boolean = true) {
+		this.memoryVariable = value;
+		if (refresh) {
+			this.refreshListeners();
+		}
+	}
+}
+
+export const BottomOptionsManager = new BottomOptManger('', undefined);
